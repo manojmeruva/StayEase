370254ed9de846ec635c66e9fb08124b1659c5bb
Manoj Meruva <manojmeruva@Manojs-MacBook-Pro.local>
2024-10-02 22:21:15 +0530

    started

---
+++ stayeaseapp/src/test/java/com/example/stayeaseapp/StayeaseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stayeaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayeaseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
